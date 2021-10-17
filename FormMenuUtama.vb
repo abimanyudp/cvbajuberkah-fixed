444347a8089b086f83233d1fbbcd50c8488dde39
Abimanyu DP <abimanyu.dp@outlook.com>
2021-10-17 13:39:24 +0700

    bisa konek menu utama ke menu login admin

--- FormMenuUtama.vb
+++ FormMenuUtama.vb
@@ -1,3 +1,7 @@
 ﻿Public Class FormMenuUtama
-
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim b = New FormLoginAdmin()
+        b.Show()
+        Me.Hide()
+    End Sub
 End Class
