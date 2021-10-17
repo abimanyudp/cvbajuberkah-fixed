a35f1790cd3dc50c2a3f22e4bb5f302af177ec93
Abimanyu DP <abimanyu.dp@outlook.com>
2021-10-17 14:13:32 +0700

    bisa konek menu utama menu login admin warehouse supp trs bisa cancel

--- FormMenuUtama.vb
+++ FormMenuUtama.vb
@@ -1,7 +1,19 @@
 ﻿Public Class FormMenuUtama
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim b = New FormLoginAdmin()
+        Dim a = New FormLoginAdmin()
+        a.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim b = New FormLoginWarehouse()
         b.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim c = New FormLoginSupplier()
+        c.Show()
         Me.Hide()
     End Sub
 End Class
