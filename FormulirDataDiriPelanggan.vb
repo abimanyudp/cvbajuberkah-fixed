f9c38064df475bc9c28c46da981ed38f96188e7b
Abimanyu DP <abimanyu.dp@outlook.com>
2021-10-18 11:55:05 +0700

    Update pembeli bisa login dan registrasi akun baru (form doang data belum).

--- FormulirDataDiriPelanggan.vb
+++ FormulirDataDiriPelanggan.vb
@@ -1,3 +1,7 @@
 ﻿Public Class FormulirDataDiriPelanggan
-
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim a = New LoginPembeli
+        a.Show()
+        Me.Close()
+    End Sub
 End Class
---
+++ FormulirRevisiDataDiriPelanggan.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormulirRevisiDataDiriPelanggan
+
+End Class
